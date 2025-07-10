135f43c5a778790d7a64f72da20adcd3601ed3c7
Shog Alharbi <146722111+ShogAlharbi@users.noreply.github.com>
2025-07-10 03:01:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/')
-def home():
-    return 'Hello, world!'
+if __name__=='__main__':
+    app.run(host='0.0.0.0', debug=True)
